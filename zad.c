b77ac4041f08c2cc5a3171f4e54e85d77e2e6e13
Talaxika <81813961+Talaxika@users.noreply.github.com>
2022-04-01 11:02:20 +0300

    Update zad.c

--- zad.c
+++ zad.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-typedef struct Node{
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct Node {
 	char data;
-	struct Node* left, *right;
-}Node;
-
-Node* createNode(char data){
+	struct Node *left, *right;
+} Node;
+Node* createNode(char data) {
 	Node* newNode;
-	if(newNode = (Node*)malloc(sizeof(Node)) == NULL)
-	{
-		printf("Error on malloc\n");
+	if((newNode=(Node*)malloc(sizeof(Node))) == NULL) {
+		printf("ERROR on malloc\n");
 		exit(-1);
 	}
-	newNode->data = daa;
+	newNode->data = data;
 	newNode->left = NULL;
 	newNode->right = NULL;
 	return newNode;
+}
+int main() {
+
+	return 0;
 }
